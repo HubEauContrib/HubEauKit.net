3396667e3360d8c59d6323e95048725b22bc7fc5
oliver254 <oliver254@hotmail.fr>
2022-08-14 16:44:51 +0200

    Add csv tests

--- src/HubEauKit.net/Vbeta/Ecoulement/Campagnes/CampagnesRequestBuilder.cs
+++ src/HubEauKit.net/Vbeta/Ecoulement/Campagnes/CampagnesRequestBuilder.cs
@@ -7,118 +7,118 @@ using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-namespace HubEauKit.Vbeta.Ecoulement.Campagnes {
-    /// <summary>Builds and executes requests for operations under \vbeta\ecoulement\campagnes</summary>
-    public class CampagnesRequestBuilder {
-        /// <summary>Path parameters for the request</summary>
-        private Dictionary<string, object> PathParameters { get; set; }
-        /// <summary>The request adapter to use to execute the requests.</summary>
-        private IRequestAdapter RequestAdapter { get; set; }
-        /// <summary>Url template to use to build the URL for the current request builder</summary>
-        private string UrlTemplate { get; set; }
+
+namespace HubEauKit.Vbeta.Ecoulement.Campagnes; 
+/// <summary>Builds and executes requests for operations under \vbeta\ecoulement\campagnes</summary>
+public class CampagnesRequestBuilder {
+    /// <summary>Path parameters for the request</summary>
+    private Dictionary<string, object> PathParameters { get; set; }
+    /// <summary>The request adapter to use to execute the requests.</summary>
+    private IRequestAdapter RequestAdapter { get; set; }
+    /// <summary>Url template to use to build the URL for the current request builder</summary>
+    private string UrlTemplate { get; set; }
+    /// <summary>
+    /// Instantiates a new CampagnesRequestBuilder and sets the default values.
+    /// <param name="pathParameters">Path parameters for the request</param>
+    /// <param name="requestAdapter">The request adapter to use to execute the requests.</param>
+    /// </summary>
+    public CampagnesRequestBuilder(Dictionary<string, object> pathParameters, IRequestAdapter requestAdapter) {
+        _ = pathParameters ?? throw new ArgumentNullException(nameof(pathParameters));
+        _ = requestAdapter ?? throw new ArgumentNullException(nameof(requestAdapter));
+        UrlTemplate = "{+baseurl}/vbeta/ecoulement/campagnes{?code_campagne,date_campagne_min,date_campagne_max,code_type_campagne,libelle_type_campagne,code_reseau,libelle_reseau,code_departement,libelle_departement,Num%C3%A9ro%20de%20la%20page,size,Liste%20des%20champs%20souhait%C3%A9s%20dans%20la%20r%C3%A9ponse%20%28fonctionnalit%C3%A9%20exp%C3%A9rimentale%29%2C%20par%20exemple%20fields%3Dcode_station%2Clocalisation}";
+        var urlTplParams = new Dictionary<string, object>(pathParameters);
+        PathParameters = urlTplParams;
+        RequestAdapter = requestAdapter;
+    }
+    /// <summary>
+    /// Instantiates a new CampagnesRequestBuilder and sets the default values.
+    /// <param name="rawUrl">The raw URL to use for the request builder.</param>
+    /// <param name="requestAdapter">The request adapter to use to execute the requests.</param>
+    /// </summary>
+    public CampagnesRequestBuilder(string rawUrl, IRequestAdapter requestAdapter) {
+        if(string.IsNullOrEmpty(rawUrl)) throw new ArgumentNullException(nameof(rawUrl));
+        _ = requestAdapter ?? throw new ArgumentNullException(nameof(requestAdapter));
+        UrlTemplate = "{+baseurl}/vbeta/ecoulement/campagnes{?code_campagne,date_campagne_min,date_campagne_max,code_type_campagne,libelle_type_campagne,code_reseau,libelle_reseau,code_departement,libelle_departement,Num%C3%A9ro%20de%20la%20page,size,Liste%20des%20champs%20souhait%C3%A9s%20dans%20la%20r%C3%A9ponse%20%28fonctionnalit%C3%A9%20exp%C3%A9rimentale%29%2C%20par%20exemple%20fields%3Dcode_station%2Clocalisation}";
+        var urlTplParams = new Dictionary<string, object>();
+        urlTplParams.Add("request-raw-url", rawUrl);
+        PathParameters = urlTplParams;
+        RequestAdapter = requestAdapter;
+    }
+    /// <summary>
+    /// Lister les campagnes
+    /// <param name="requestConfiguration">Configuration for the request such as headers, query parameters, and middleware options.</param>
+    /// </summary>
+    public RequestInformation CreateGetRequestInformation(Action<CampagnesRequestBuilderGetRequestConfiguration> requestConfiguration = default) {
+        var requestInfo = new RequestInformation {
+            HttpMethod = Method.GET,
+            UrlTemplate = UrlTemplate,
+            PathParameters = PathParameters,
+        };
+        requestInfo.Headers.Add("Accept", "application/json");
+        if (requestConfiguration != null) {
+            var requestConfig = new CampagnesRequestBuilderGetRequestConfiguration();
+            requestConfiguration.Invoke(requestConfig);
+            requestInfo.AddQueryParameters(requestConfig.QueryParameters);
+            requestInfo.AddRequestOptions(requestConfig.Options);
+            requestInfo.AddHeaders(requestConfig.Headers);
+        }
+        return requestInfo;
+    }
+    /// <summary>
+    /// Lister les campagnes
+    /// <param name="cancellationToken">Cancellation token to use when cancelling requests</param>
+    /// <param name="requestConfiguration">Configuration for the request such as headers, query parameters, and middleware options.</param>
+    /// <param name="responseHandler">Response handler to use in place of the default response handling provided by the core service</param>
+    /// </summary>
+    public async Task<HubEauKit.Models.CampagneResult> GetAsync(Action<CampagnesRequestBuilderGetRequestConfiguration> requestConfiguration = default, IResponseHandler responseHandler = default, CancellationToken cancellationToken = default) {
+        var requestInfo = CreateGetRequestInformation(requestConfiguration);
+        var errorMapping = new Dictionary<string, ParsableFactory<IParsable>> {
+            {"400", CampagneResult.CreateFromDiscriminatorValue},
+            {"404", CampagneResult.CreateFromDiscriminatorValue},
+            {"500", CampagneResult.CreateFromDiscriminatorValue},
+        };
+        return await RequestAdapter.SendAsync<HubEauKit.Models.CampagneResult>(requestInfo, HubEauKit.Models.CampagneResult.CreateFromDiscriminatorValue, responseHandler, errorMapping, cancellationToken);
+    }
+    /// <summary>Lister les campagnes</summary>
+    public class CampagnesRequestBuilderGetQueryParameters {
+        /// <summary>Code(s) Onde de la campagne. Identfiant numérique auto incrémenté non signifiant, si plusieurs codes les séparer par une virgule, nombre maximum de codes = 20</summary>
+        public int?[] Code_campagne { get; set; }
+        /// <summary>Code(s) INSEE du département associé à la campagne d&apos;observation. Séparer les valeurs par des virgules. Nombre maximum de valeurs : 200</summary>
+        public string[] Code_departement { get; set; }
+        /// <summary>Code(s) Sandre du réseau associé à la campagne d&apos;observation. Valeurs possibles : https://api.sandre.eaufrance.fr/referentiels/v1/dc.json. Séparer les valeurs par des virgules. Nombre maximum de valeurs : 200</summary>
+        public string[] Code_reseau { get; set; }
+        /// <summary>Code du type de campagne d&apos;observation. Valeurs possibles : 1 / 2</summary>
+        public int?[] Code_type_campagne { get; set; }
+        /// <summary>Date maximale de la campagne (format YYYY-MM-DD) : renvoie les campagnes pour lesquelles date_campagne &lt;= date_campagne_max</summary>
+        public DateTimeOffset? Date_campagne_max { get; set; }
+        /// <summary>Date minimale de la campagne (format YYYY-MM-DD) : renvoie les campagnes pour lesquelles date_campagne &gt;= date_campagne_min</summary>
+        public DateTimeOffset? Date_campagne_min { get; set; }
+        /// <summary>Libellé(s) INSEE du département associé à la campagne d&apos;observation. Séparer les valeurs par des virgules. Nombre maximum de valeurs : 200</summary>
+        public string[] Libelle_departement { get; set; }
+        /// <summary>Libellé(s) Sandre du réseau associé à la campagne d&apos;observation. Valeurs possibles : https://api.sandre.eaufrance.fr/referentiels/v1/dc.json. Séparer les valeurs par des virgules. Nombre maximum de valeurs : 200</summary>
+        public string[] Libelle_reseau { get; set; }
+        /// <summary>Type de campagne d&apos;observation. Valeurs possibles : Usuelle / Complémentaire</summary>
+        public string[] Libelle_type_campagne { get; set; }
+        [QueryParameter("Liste%20des%20champs%20souhait%C3%A9s%20dans%20la%20r%C3%A9ponse%20%28fonctionnalit%C3%A9%20exp%C3%A9rimentale%29%2C%20par%20exemple%20fields%3Dcode_station%2Clocalisation")]
+        public string Listedeschampssouhaitsdanslarponsefonctionnalitexprimentaleparexemplefieldscode_stationlocalisation { get; set; }
+        [QueryParameter("Num%C3%A9ro%20de%20la%20page")]
+        public int? Numrodelapage { get; set; }
+        public int? Size { get; set; }
+    }
+    /// <summary>Configuration for the request such as headers, query parameters, and middleware options.</summary>
+    public class CampagnesRequestBuilderGetRequestConfiguration {
+        /// <summary>Request headers</summary>
+        public IDictionary<string, string> Headers { get; set; }
+        /// <summary>Request options</summary>
+        public IList<IRequestOption> Options { get; set; }
+        /// <summary>Request query parameters</summary>
+        public CampagnesRequestBuilderGetQueryParameters QueryParameters { get; set; } = new CampagnesRequestBuilderGetQueryParameters();
         /// <summary>
-        /// Instantiates a new CampagnesRequestBuilder and sets the default values.
-        /// <param name="pathParameters">Path parameters for the request</param>
-        /// <param name="requestAdapter">The request adapter to use to execute the requests.</param>
+        /// Instantiates a new campagnesRequestBuilderGetRequestConfiguration and sets the default values.
         /// </summary>
-        public CampagnesRequestBuilder(Dictionary<string, object> pathParameters, IRequestAdapter requestAdapter) {
-            _ = pathParameters ?? throw new ArgumentNullException(nameof(pathParameters));
-            _ = requestAdapter ?? throw new ArgumentNullException(nameof(requestAdapter));
-            UrlTemplate = "{+baseurl}/vbeta/ecoulement/campagnes{?code_campagne,date_campagne_min,date_campagne_max,code_type_campagne,libelle_type_campagne,code_reseau,libelle_reseau,code_departement,libelle_departement,Num%C3%A9ro%20de%20la%20page,size,Liste%20des%20champs%20souhait%C3%A9s%20dans%20la%20r%C3%A9ponse%20%28fonctionnalit%C3%A9%20exp%C3%A9rimentale%29%2C%20par%20exemple%20fields%3Dcode_station%2Clocalisation}";
-            var urlTplParams = new Dictionary<string, object>(pathParameters);
-            PathParameters = urlTplParams;
-            RequestAdapter = requestAdapter;
-        }
-        /// <summary>
-        /// Instantiates a new CampagnesRequestBuilder and sets the default values.
-        /// <param name="rawUrl">The raw URL to use for the request builder.</param>
-        /// <param name="requestAdapter">The request adapter to use to execute the requests.</param>
-        /// </summary>
-        public CampagnesRequestBuilder(string rawUrl, IRequestAdapter requestAdapter) {
-            if(string.IsNullOrEmpty(rawUrl)) throw new ArgumentNullException(nameof(rawUrl));
-            _ = requestAdapter ?? throw new ArgumentNullException(nameof(requestAdapter));
-            UrlTemplate = "{+baseurl}/vbeta/ecoulement/campagnes{?code_campagne,date_campagne_min,date_campagne_max,code_type_campagne,libelle_type_campagne,code_reseau,libelle_reseau,code_departement,libelle_departement,Num%C3%A9ro%20de%20la%20page,size,Liste%20des%20champs%20souhait%C3%A9s%20dans%20la%20r%C3%A9ponse%20%28fonctionnalit%C3%A9%20exp%C3%A9rimentale%29%2C%20par%20exemple%20fields%3Dcode_station%2Clocalisation}";
-            var urlTplParams = new Dictionary<string, object>();
-            urlTplParams.Add("request-raw-url", rawUrl);
-            PathParameters = urlTplParams;
-            RequestAdapter = requestAdapter;
-        }
-        /// <summary>
-        /// Lister les campagnes
-        /// <param name="requestConfiguration">Configuration for the request such as headers, query parameters, and middleware options.</param>
-        /// </summary>
-        public RequestInformation CreateGetRequestInformation(Action<CampagnesRequestBuilderGetRequestConfiguration> requestConfiguration = default) {
-            var requestInfo = new RequestInformation {
-                HttpMethod = Method.GET,
-                UrlTemplate = UrlTemplate,
-                PathParameters = PathParameters,
-            };
-            requestInfo.Headers.Add("Accept", "application/json");
-            if (requestConfiguration != null) {
-                var requestConfig = new CampagnesRequestBuilderGetRequestConfiguration();
-                requestConfiguration.Invoke(requestConfig);
-                requestInfo.AddQueryParameters(requestConfig.QueryParameters);
-                requestInfo.AddRequestOptions(requestConfig.Options);
-                requestInfo.AddHeaders(requestConfig.Headers);
-            }
-            return requestInfo;
-        }
-        /// <summary>
-        /// Lister les campagnes
-        /// <param name="cancellationToken">Cancellation token to use when cancelling requests</param>
-        /// <param name="requestConfiguration">Configuration for the request such as headers, query parameters, and middleware options.</param>
-        /// <param name="responseHandler">Response handler to use in place of the default response handling provided by the core service</param>
-        /// </summary>
-        public async Task<HubEauKit.Models.CampagneResult> GetAsync(Action<CampagnesRequestBuilderGetRequestConfiguration> requestConfiguration = default, IResponseHandler responseHandler = default, CancellationToken cancellationToken = default) {
-            var requestInfo = CreateGetRequestInformation(requestConfiguration);
-            var errorMapping = new Dictionary<string, ParsableFactory<IParsable>> {
-                {"400", CampagneResult.CreateFromDiscriminatorValue},
-                {"404", CampagneResult.CreateFromDiscriminatorValue},
-                {"500", CampagneResult.CreateFromDiscriminatorValue},
-            };
-            return await RequestAdapter.SendAsync<HubEauKit.Models.CampagneResult>(requestInfo, HubEauKit.Models.CampagneResult.CreateFromDiscriminatorValue, responseHandler, errorMapping, cancellationToken);
-        }
-        /// <summary>Lister les campagnes</summary>
-        public class CampagnesRequestBuilderGetQueryParameters {
-            /// <summary>Code(s) Onde de la campagne. Identfiant numérique auto incrémenté non signifiant, si plusieurs codes les séparer par une virgule, nombre maximum de codes = 20</summary>
-            public int?[] Code_campagne { get; set; }
-            /// <summary>Code(s) INSEE du département associé à la campagne d&apos;observation. Séparer les valeurs par des virgules. Nombre maximum de valeurs : 200</summary>
-            public string[] Code_departement { get; set; }
-            /// <summary>Code(s) Sandre du réseau associé à la campagne d&apos;observation. Valeurs possibles : https://api.sandre.eaufrance.fr/referentiels/v1/dc.json. Séparer les valeurs par des virgules. Nombre maximum de valeurs : 200</summary>
-            public string[] Code_reseau { get; set; }
-            /// <summary>Code du type de campagne d&apos;observation. Valeurs possibles : 1 / 2</summary>
-            public int?[] Code_type_campagne { get; set; }
-            /// <summary>Date maximale de la campagne (format YYYY-MM-DD) : renvoie les campagnes pour lesquelles date_campagne &lt;= date_campagne_max</summary>
-            public DateTimeOffset? Date_campagne_max { get; set; }
-            /// <summary>Date minimale de la campagne (format YYYY-MM-DD) : renvoie les campagnes pour lesquelles date_campagne &gt;= date_campagne_min</summary>
-            public DateTimeOffset? Date_campagne_min { get; set; }
-            /// <summary>Libellé(s) INSEE du département associé à la campagne d&apos;observation. Séparer les valeurs par des virgules. Nombre maximum de valeurs : 200</summary>
-            public string[] Libelle_departement { get; set; }
-            /// <summary>Libellé(s) Sandre du réseau associé à la campagne d&apos;observation. Valeurs possibles : https://api.sandre.eaufrance.fr/referentiels/v1/dc.json. Séparer les valeurs par des virgules. Nombre maximum de valeurs : 200</summary>
-            public string[] Libelle_reseau { get; set; }
-            /// <summary>Type de campagne d&apos;observation. Valeurs possibles : Usuelle / Complémentaire</summary>
-            public string[] Libelle_type_campagne { get; set; }
-            [QueryParameter("Liste%20des%20champs%20souhait%C3%A9s%20dans%20la%20r%C3%A9ponse%20%28fonctionnalit%C3%A9%20exp%C3%A9rimentale%29%2C%20par%20exemple%20fields%3Dcode_station%2Clocalisation")]
-            public string Listedeschampssouhaitsdanslarponsefonctionnalitexprimentaleparexemplefieldscode_stationlocalisation { get; set; }
-            [QueryParameter("Num%C3%A9ro%20de%20la%20page")]
-            public int? Numrodelapage { get; set; }
-            public int? Size { get; set; }
-        }
-        /// <summary>Configuration for the request such as headers, query parameters, and middleware options.</summary>
-        public class CampagnesRequestBuilderGetRequestConfiguration {
-            /// <summary>Request headers</summary>
-            public IDictionary<string, string> Headers { get; set; }
-            /// <summary>Request options</summary>
-            public IList<IRequestOption> Options { get; set; }
-            /// <summary>Request query parameters</summary>
-            public CampagnesRequestBuilderGetQueryParameters QueryParameters { get; set; } = new CampagnesRequestBuilderGetQueryParameters();
-            /// <summary>
-            /// Instantiates a new campagnesRequestBuilderGetRequestConfiguration and sets the default values.
-            /// </summary>
-            public CampagnesRequestBuilderGetRequestConfiguration() {
-                Options = new List<IRequestOption>();
-                Headers = new Dictionary<string, string>();
-            }
+        public CampagnesRequestBuilderGetRequestConfiguration() {
+            Options = new List<IRequestOption>();
+            Headers = new Dictionary<string, string>();
         }
     }
 }
--- test/HubEauKit.Tests.Integration/EcoulementClientTests.cs
+++ test/HubEauKit.Tests.Integration/EcoulementClientTests.cs
@@ -1,37 +1,174 @@
-﻿using FluentAssertions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+using FluentAssertions;
+using System.Globalization;
 
-namespace HubEauKit.Tests.Integration
+namespace HubEauKit.Tests.Integration;
+
+public class EcoulementClientTests
 {
-    public class EcoulementClientTests
+    private readonly EcoulementClient _client;
+
+    public EcoulementClientTests()
     {
-        private readonly EcoulementClient _client;
 
-        public EcoulementClientTests()
+
+        _client = new EcoulementClient(new HttpClientRequestAdapter(new AnonymousAuthenticationProvider()));
+    }
+
+    [Fact]
+    public async Task GetStationsTest()
+    {
+        var response = await _client.Vbeta.Ecoulement.Stations.GetAsync(q =>
         {
+            q.QueryParameters.Size = 2;
+            q.QueryParameters.Libelle_cours_eau = new string[] { "La Moselle" };
+            q.QueryParameters.Sort = "Code_station";
 
+        });
 
-            _client = new EcoulementClient(new HttpClientRequestAdapter(new AnonymousAuthenticationProvider()));
+        response.Data.Should().HaveCount(2);
+        response.Data[0].Code_bassin.Should().Be("02");
+        response.Data[0].Code_commune.Should().Be("88081");
+        response.Data[0].Code_cours_eau.Should().Be("A---0060");
+        response.Data[0].Code_departement.Should().Be("88");
+        response.Data[0].Code_epsg_station.Should().Be("2154");
+        response.Data[0].Code_projection_station.Should().Be("26");
+        response.Data[0].Code_region.Should().Be("44");
+        response.Data[0].Code_station.Should().Be("A4000001");
+        response.Data[0].Coordonnee_x_station.Should().Be(989351.1d);
+        response.Data[0].Coordonnee_y_station.Should().Be(6761643);
+        response.Data[0].Etat_station.Should().Be("Active");
+        response.Data[0].Latitude.Should().Be(47.891296707d);
+        response.Data[0].Libelle_bassin.Should().Be("Rhin-Meuse");
+        response.Data[0].Libelle_commune.Should().Be("BUSSANG");
+        response.Data[0].Libelle_cours_eau.Should().Be("La Moselle");
+        response.Data[0].Libelle_departement.Should().Be("Vosges");
+        response.Data[0].Libelle_projection_station.Should().Be("RGF93 / Lambert 93");
+        response.Data[0].Libelle_region.Should().Be("Grand Est");
+        response.Data[0].Libelle_station.Should().Be("La Moselle a Bussang");
+        response.Data[0].Longitude.Should().Be(6.873374276d);
+    }
+
+    [Fact]
+    public async Task GetObservationsTest()
+    {
+        var response = await _client.Vbeta.Ecoulement.Observations.GetAsync(q =>
+        {
+            q.QueryParameters.Size = 10;
+
+        });
+
+        response.Data.Should().NotBeNull();
+    }
+
+    [Fact]
+    public async Task GetObservationsCsvTest()
+    {
+        var stream = await _client.Vbeta.Ecoulement.Csv.GetAsync(async q =>
+        {
+            q.QueryParameters.Size = 2;
+        });
+
+        var configuration = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            Delimiter = ";"
+        };
+
+        List<ObservationCsv> records;
+
+        using (StreamReader sr = new StreamReader(stream))
+        using(CsvReader cr = new CsvReader(sr, configuration))
+        {
+            records = cr.GetRecords<ObservationCsv>().ToList();
         }
 
-        [Fact]
-        public async Task GetStations()
-        {
-            var response = await _client.Vbeta.Ecoulement.Stations.GetAsync(q =>
-            {
-                q.QueryParameters.Size = 2;
-                q.QueryParameters.Libelle_cours_eau = new string[] { "La Moselle" };
-                q.QueryParameters.Sort = "Code_station";
 
-            });
-            response.Data.Should().HaveCount(2);
-            response.Data[0].Libelle_cours_eau.Should().Be("La Moselle");
-            response.Data[0].Code_cours_eau.Should().Be("A---0060");
-            response.Data[0].Code_station.Should().Be("A4000001");
-        }
+        records.Should().HaveCount(2);
+        records[0].Code_station.Should().Be("K3640002");
+        records[0].Libelle_station.Should().Be("Ruisseau du Pont Aubert à Mars-Sur-Allier");
+        records[0].Uri_station.Should().Be("http://id.eaufrance.fr/StationHydro/K3640002");
+        records[0].Code_departement.Should().Be("58");
+        records[0].Libelle_departement.Should().Be("Nièvre");
+        records[0].Code_commune.Should().Be("58158");
+        records[0].Libelle_commune.Should().Be("MARS-SUR-ALLIER");
+        records[0].Code_region.Should().Be("27");
+        records[0].Libelle_region.Should().Be("Bourgogne-Franche-Comté");
+        records[0].Code_bassin.Should().Be("04");
+        records[0].Libelle_bassin.Should().Be("Loire-Bretagne");
+        records[0].Coordonnee_x_station.Should().Be(706424.9d);
+        records[0].Coordonnee_y_station.Should().Be(6638959.5d);
+        records[0].Code_projection_station.Should().Be("26");
+        records[0].Libelle_projection_station.Should().Be("RGF93 / Lambert 93");
+        records[0].Code_cours_eau.Should().Be("K3645000");
+        records[0].Libelle_cours_eau.Should().Be("le Pont Aubert");
+        records[0].Uri_cours_eau.Should().Be("http://id.eaufrance.fr/CoursEau_Carthage2017/K3645000");
+        records[0].Code_campagne.Should().Be("96325");
+        records[0].Code_reseau.Should().Be("0000000134");
+        records[0].Libelle_reseau.Should().Be("Onde OFB");
+        records[0].Uri_reseau.Should().Be("http://id.eaufrance.fr/dc/0000000134");
+        records[0].Code_ecoulement.Should().Be("3");
+        records[0].Latitude.Should().Be(46.85076724427001d);
+        records[0].Longitude.Should().Be(3.0843202694409513);
+    }
+
+    public class ObservationCsv
+    {
+        [Name("code_bassin")]
+        public string Code_bassin { get; set; }
+        [Name("code_campagne")]
+        public string Code_campagne { get; set; }
+        [Name("code_commune")]
+        public string Code_commune { get; set; }
+        [Name("code_cours_eau")]
+        public string Code_cours_eau { get; set; }
+        [Name("code_departement")]
+        public string Code_departement { get; set; }
+        [Name("code_ecoulement")]
+        public string Code_ecoulement { get; set; }
+        [Name("code_projection_station")]
+        public string Code_projection_station { get; set; }
+        [Name("code_region")]
+        public string Code_region { get; set; }
+        [Name("code_reseau")]
+        public string Code_reseau { get; set; }
+        [Name("code_station")]
+        public string Code_station { get; set; }
+        [Name("coordonnee_x_station")]
+        public double? Coordonnee_x_station { get; set; }
+        [Name("coordonnee_y_station")]
+        public double? Coordonnee_y_station { get; set; }
+        [Name("date_observation")]
+        public DateTimeOffset? Date_observation { get; set; }
+        [Name("latitude")]
+        public double? Latitude { get; set; }
+        [Name("libelle_bassin")]
+        public string Libelle_bassin { get; set; }
+        [Name("libelle_commune")]
+        public string Libelle_commune { get; set; }
+        [Name("libelle_cours_eau")]
+        public string Libelle_cours_eau { get; set; }
+        [Name("libelle_departement")]
+        public string Libelle_departement { get; set; }
+        [Name("libelle_ecoulement")]
+        public string Libelle_ecoulement { get; set; }
+        [Name("libelle_projection_station")]
+        public string Libelle_projection_station { get; set; }
+        [Name("libelle_region")]
+        public string Libelle_region { get; set; }
+        [Name("libelle_reseau")]
+        public string Libelle_reseau { get; set; }
+        [Name("libelle_station")]
+        public string Libelle_station { get; set; }
+        [Name("longitude")]
+        public double? Longitude { get; set; }
+        [Name("uri_cours_eau")]
+        public string Uri_cours_eau { get; set; }
+        [Name("uri_reseau")]
+        public string Uri_reseau { get; set; }
+        [Name("uri_station")]
+        public string Uri_station { get; set; }
+
     }
 }
